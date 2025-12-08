a346015680f7ba2277e72deed0a3e22941055fe2
kolleennz <kolleennz@gmail.com>
2025-12-08 11:55:21 +0100

    Save local changes before rebase

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from post import Post
 app_user_one = User("alamba.gmail.com", "Collins Uket", "Pwd","Devops Engineer")
 app_user_one.get_user_info()
 
-app_user_two = User("collins.gmail.com", "Collins Alamba", "Pwd","Devops Engineer")
+app_user_two = User("collinsuketjnr@gmail.com", "Collins Alamba", "Pwd","Devops Engineer")
 app_user_two.get_user_info()
 
 new_post = Post("on a secret mission today", app_user_two.name)
